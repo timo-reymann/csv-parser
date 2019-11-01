ba0caa54ab503b5491423f2276930a4c206d5718
Timo Reymann <mail@timo-reymann.de>
2019-11-01 13:49:01 +0100

    Release 3.1.0

---
+++ release.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+# Verify release type is present
+releaseType=$1
+if [ -z "$1" ]
+then
+  echo "Please specify release type!"
+  exit 2;
+fi
+
+# Increase tag using git-semver-tag (https://github.com/timo-reymann/git-semver-tag)
+tag=$(git semver-tag -level "$1" 2>&1)
+
+# Set maven version
+mvn -B versions:set -DnewVersion=$tag
+
+rm pom.xml.versionsBackup
+
+# Clean and deploy to central
+mvn clean deploy
+
+# Update README version
+sed "s/<version>.*<\/version\>/<version>${tag}<\/version>/g" README.md > README.md
+
+git stage .
+git commit -m "Release ${tag}"
+git push
+git push --tags
